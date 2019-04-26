29f16780b1cf65efbd9e5492bd58e874f3fe9716
razgaou <rachid.azgaou.us@gmail.com>
2019-04-26 11:50:38 +0200

    Init Commit

---
+++ Elte_detector/Elte_detector/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elte_detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
