6b7f72f8e712fcd4e8a1e103245d076c7ed10af5
razgaou <rachid.azgaou.us@gmail.com>
2019-04-26 17:50:16 +0200

    replace json params by app.config , + minor changes in the main

--- Elte_detector/Elte_detector/Program.cs
+++ Elte_detector/Elte_detector/Program.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+
+
+
 
 namespace Elte_detector
 {
     class Program
     {
+
+        public static System.Collections.Specialized.NameValueCollection AppSettings { get; }
+
         static void Main(string[] args)
         {
 
+
+
             Console.WriteLine("ELTE detector tool - Rachid AZGAOU 2019");
+            Console.WriteLine("Configuration file reading .. ");
+            Console.WriteLine(Application.StartupPath);
+            Console.WriteLine(ConfigurationManage. );
             Console.ReadLine();
 
 
